42c5400b93a1da5415d00c0fbb17d5e297361edc
Kamronbek541 <128393982+Kamronbek541@users.noreply.github.com>
2025-03-31 19:06:33 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,20 +1,18 @@
 
 
+document.getElementById('contact-form').addEventListener('submit', function(event) {
+    event.preventDefault(); // Prevent the default form submission behavior
 
-
-
-// EMAIL LOGIC TO DO
-(function(){
-    emailjs.init("YOUR_USER_ID"); // Replace with your EmailJS User ID
-})();
-
-document.getElementById("contact-form").addEventListener("submit", function(event) {
-    event.preventDefault();
-    
-    emailjs.sendForm("YOUR_SERVICE_ID", "YOUR_TEMPLATE_ID", this)
+    // Send the email using EmailJS
+    emailjs.sendForm('service_9w1opvg', 'template_23y71pp', this)
         .then(function(response) {
-            alert("Message sent successfully!");
+            alert('Email successfully sent!'); // Show success alert
+            document.getElementById('contact-form').reset(); // Reset the form fields
+            setTimeout(function() { // Set a timeout to refresh after a short delay
+                window.location.reload(); // Refresh the page
+            }, 2000); // Wait for 2 seconds before refreshing
         }, function(error) {
-            alert("Failed to send message: " + error.text);
+            alert('Failed to send email: ' + JSON.stringify(error)); // Show error alert
         });
 });
+
